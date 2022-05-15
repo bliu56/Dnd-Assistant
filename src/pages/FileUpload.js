dc7fc665831eb9d779061430c4355f61e6482a8d
ddominiqUCSC <ddominiq@ucsc.edu>
2022-05-15 02:43:37 -0700

    Upload to Firebase

--- src/pages/FileUpload.js
+++ src/pages/FileUpload.js
@@ -1,8 +1,16 @@
 import './FileUpload.css';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import { auth } from '../firebase-config';
+import userEvent from '@testing-library/user-event';
+import { doc, getFirestore, setDoc } from "firebase/firestore"; 
+
+const db=getFirestore();
+
+
 
 function FileUpload(){
 
+    const [extratedData, setData] = useState();
 
     const [file, fileToChange] = useState();
 
@@ -12,19 +20,33 @@ function FileUpload(){
         fileToChange(event.target.files[0]);
     }
 
+ 
+
     const FileUploadHandler =() =>  
     {
+                    const reader = new FileReader();
+                    reader.readAsText(file);
+                    reader.onload = function() {
+                        var data = JSON.parse(reader.result);
+                        //var user=auth.currentUser;
+                        //const uid=user.uid;
 
-        console.log(file)
-        const reader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = function() {
-            var data = JSON.parse(reader.result);
+                        const uid="BWLSuKe0yMw206XTBrxC";
+                        const path="User/"+uid+"/characters"
+                        const fileName=file.name;
+                        //setDoc("User/BWLSuKe0yMw206XTBrxC/characters", "test");
+                        //const colRef=collection()
+                        setDoc(doc(db,path, fileName), data);
+                        
 
-          };
-          
+                        //SEPERATE folder should be named w/ uid
+                        //ping/connect firebase w/ specific userID
+                        //navigate to their specific table for characters
+                        //push json to firebase folder
+                        
+                    };
+
     };
- 
 
     return(
 
