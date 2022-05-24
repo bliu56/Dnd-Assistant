3a6e79681a48eb9730252ea0caa48b620d2040c1
Mateo Rodriguez Reyes <MattRodds6@gmail.com>
2022-05-23 20:32:48 -0700

    added some hp stuff

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,45 @@
 import './Home.css';
+import {Carousel} from 'react-bootstrap';
+import logo1 from '../img/Pen-and-Dice.jpg'
 
 function Home(){
     return(
+        <Carousel>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={logo1}
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h1>Create a Character</h1>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Second slide&bg=282c34"
+                alt="Second slide"
+                />
 
-            <div>Home</div>
+                <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Third slide&bg=20232a"
+                alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 }
 
